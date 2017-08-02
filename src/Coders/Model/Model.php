afd48f93f4c0e7603bfea7a7971b4c91dd0d2420
Damien Burke <damien.burke@jamesandjames.com>
2017-08-02 14:54:04 +0100

    StyleCI changes
    Why oh why doesn't StyleCI display line numbers...

--- src/Coders/Model/Model.php
+++ src/Coders/Model/Model.php
@@ -363,6 +363,7 @@ class Model
         if ($andRemovePrefix) {
             return $this->removeTablePrefix($this->blueprint->table());
         }
+        
         return $this->blueprint->table();
     }
 
