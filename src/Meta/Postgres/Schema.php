f59295e927b678bd5349f0445843a5e252857364
gareth-ib <gareth.pursehouse.ib@gmail.com>
2018-06-15 19:54:56 +0000

    Apply fixes from StyleCI

--- src/Meta/Postgres/Schema.php
+++ src/Meta/Postgres/Schema.php
@@ -119,54 +119,54 @@ WHERE
             'table'     => $table,
         ];
 
-/*/
-
-"table_catalog" => "my_database_name"
-"table_schema" => "my_schema_name"
-"table_name" => "my_table_name"
-"column_name" => "my_field_name"
-"ordinal_position" => 1
-"column_default" => "nextval('my_field_name_id_seq'::regclass)"
-"is_nullable" => "NO"
-"data_type" => "integer"
-"character_maximum_length" => null
-"character_octet_length" => null
-"numeric_precision" => 32
-"numeric_precision_radix" => 2
-"numeric_scale" => 0
-"datetime_precision" => null
-"interval_type" => null
-"interval_precision" => null
-"character_set_catalog" => null
-"character_set_schema" => null
-"character_set_name" => null
-"collation_catalog" => null
-"collation_schema" => null
-"collation_name" => null
-"domain_catalog" => null
-"domain_schema" => null
-"domain_name" => null
-"udt_catalog" => "my_database_name"
-"udt_schema" => "pg_catalog"
-"udt_name" => "int4"
-"scope_catalog" => null
-"scope_schema" => null
-"scope_name" => null
-"maximum_cardinality" => null
-"dtd_identifier" => "1"
-"is_self_referencing" => "NO"
-"is_identity" => "NO"
-"identity_generation" => null
-"identity_start" => null
-"identity_increment" => null
-"identity_maximum" => null
-"identity_minimum" => null
-"identity_cycle" => "NO"
-"is_generated" => "NEVER"
-"generation_expression" => null
-"is_updatable" => "YES"
-
-/**/
+        /*/
+        
+        "table_catalog" => "my_database_name"
+        "table_schema" => "my_schema_name"
+        "table_name" => "my_table_name"
+        "column_name" => "my_field_name"
+        "ordinal_position" => 1
+        "column_default" => "nextval('my_field_name_id_seq'::regclass)"
+        "is_nullable" => "NO"
+        "data_type" => "integer"
+        "character_maximum_length" => null
+        "character_octet_length" => null
+        "numeric_precision" => 32
+        "numeric_precision_radix" => 2
+        "numeric_scale" => 0
+        "datetime_precision" => null
+        "interval_type" => null
+        "interval_precision" => null
+        "character_set_catalog" => null
+        "character_set_schema" => null
+        "character_set_name" => null
+        "collation_catalog" => null
+        "collation_schema" => null
+        "collation_name" => null
+        "domain_catalog" => null
+        "domain_schema" => null
+        "domain_name" => null
+        "udt_catalog" => "my_database_name"
+        "udt_schema" => "pg_catalog"
+        "udt_name" => "int4"
+        "scope_catalog" => null
+        "scope_schema" => null
+        "scope_name" => null
+        "maximum_cardinality" => null
+        "dtd_identifier" => "1"
+        "is_self_referencing" => "NO"
+        "is_identity" => "NO"
+        "identity_generation" => null
+        "identity_start" => null
+        "identity_increment" => null
+        "identity_maximum" => null
+        "identity_minimum" => null
+        "identity_cycle" => "NO"
+        "is_generated" => "NEVER"
+        "generation_expression" => null
+        "is_updatable" => "YES"
+        
+        /**/
 
         $rows = $this->arraify($this->connection->select($sql, $params));
 
