605d46f191d54d6dd7be28399e03fe8f20d0790e
gareth-ib <gareth.pursehouse.ib@gmail.com>
2018-06-26 22:40:11 +0000

    Apply fixes from StyleCI

--- src/Coders/Model/Model.php
+++ src/Coders/Model/Model.php
@@ -445,10 +445,11 @@ class Model
      */
     public function withNamespace($namespace)
     {
-        $namespaceSchema = $this->config( 'namespace_schema', false );
+        $namespaceSchema = $this->config('namespace_schema', false);
 
-        if( $namespaceSchema )
-            $namespace .= "\\".$this->factory->transformSchemaToNamespace( $this->getSchema() );
+        if ($namespaceSchema) {
+            $namespace .= '\\'.$this->factory->transformSchemaToNamespace($this->getSchema());
+        }
 
         $this->namespace = $namespace;
 
