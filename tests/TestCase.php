1f10ed43802b2a07ee7f308e9a87ca875cfb02ba
Cristian Llanos <cristianllanos@outlook.com>
2020-12-06 21:35:15 -0500

    Raised testing support to PHP 7.3 and PHP 7.4

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,12 @@
  * Created by Cristian.
  * Date: 16/10/16 12:49 PM.
  */
-class TestCase extends PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
     /**
      * Clean up the testing environment before the next test.
      */
-    protected function tearDown()
+    protected function tearDown(): void
     {
         if (class_exists('Mockery')) {
             Mockery::close();
