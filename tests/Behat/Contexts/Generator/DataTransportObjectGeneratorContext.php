6215a65e8334034fa870090a0efcd65bb2d2e573
Adam Lenda <adamlenda@gmail.com>
2021-05-18 16:42:13 -0400

    removed dead test code

--- tests/Behat/Contexts/Generator/DataTransportObjectGeneratorContext.php
+++ tests/Behat/Contexts/Generator/DataTransportObjectGeneratorContext.php
@@ -2,24 +2,12 @@
 
 namespace Tests\Behat\Contexts\Generator;
 
-use Behat\Behat\Tester\Exception\PendingException;
-use Reliese\Generator\DataTransport\DataTransportGenerator;
 use Reliese\Generator\DataTransport\DataTransportObjectGenerator;
-use Tests\Behat\Contexts\FeatureContext;
 /**
  * Class DataTransportObjectGeneratorContext
  */
 class DataTransportObjectGeneratorContext extends GeneratorContexts
 {
-    public function getDataTransportObjectGenerator(): DataTransportObjectGenerator
-    {
-        return new DataTransportObjectGenerator(
-            $this->getConfigurationContexts()
-                 ->getDataTransportObjectGeneratorConfigurationContext()
-                 ->getDataTransportObjectGeneratorConfiguration()
-        );
-    }
-
     /**
      * @When /^DataTransportObjectGenerator generates Abstract Dto from Schema "([^"]*)" Table "([^"]*)"$/
      */
