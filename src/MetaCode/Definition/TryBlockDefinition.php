388afe1a5e800eed024607a92b16346225c0300e
Adam Lenda <adamlenda@gmail.com>
2021-05-20 04:49:46 -0400

    Added TryBlock Definition

---
+++ src/MetaCode/Definition/TryBlockDefinition.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Reliese\MetaCode\Definition;
+
+use Illuminate\Support\Str;
+use Reliese\MetaCode\Format\IndentationProviderInterface;
+use Reliese\MetaCode\Tool\ClassNameTool;
+use function sprintf;
+/**
+ * Class TryBlockDefinition
+ */
+class TryBlockDefinition extends StatementBlockDefinition
+{
+    public function __construct()
+    {
+        parent::__construct(
+            new RawStatementDefinition("try"),
+            null
+        );
+    }
+
+    public function addCatchStatements(\Reliese\MetaCode\Enum\PhpTypeEnum $exceptionType,
+        string $exceptionVariableName,
+        StatementDefinitionCollectionInterface $catchStatementDefinitionCollection
+    ): static {
+        $catchStatement = new RawStatementDefinition(
+            sprintf(
+                "catch (%s \$%s) ",
+                ClassNameTool::fullyQualifiedName($exceptionType->toDeclarationType()),
+                $exceptionVariableName
+            )
+        );
+
+        $this->setBlockSuffixStatement(
+            (new StatementBlockDefinition(
+                $catchStatement,
+                $catchStatementDefinitionCollection
+            ))
+        );
+
+        return $this;
+    }
+}
